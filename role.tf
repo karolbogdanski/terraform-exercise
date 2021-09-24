54751324d0ad3384a0da37d8a6492c0b1efd9969
karolbogdanski <karol.bogdanski3@gmail.com>
2021-09-24 12:47:58 +0200

    role working

--- role.tf
+++ role.tf
@@ -1,32 +1,46 @@
-resource "aws_iam_role" "read-s3-role" {
-  name = var.role_name
-
-  assume_role_policy = jsonencode({
-
+resource "aws_iam_policy" "read_s3_policy" {
+  policy = jsonencode({
     "Version": "2012-10-17",
-    
     "Statement": [
-
         {
-            "Sid": "VisualEditor0",
             "Effect": "Allow",
             "Action": [
-                "s3:GetObject",
-                "s3:ListBucket"
+                "s3:Get*",
+                "s3:List*"
             ],
-            "Resource": [
-                "arn:aws:s3:::my-terra-bucket/*"
-            ]
-            "permissions_boundary": [
-                "arn:aws:iam::113304117666:policy/DefaultBoundaryPolicy"
-            ]
+            "Resource": "arn:aws:s3:::my-terra-bucket/*"
         }
+    ]
+  })
+}
+
+resource "aws_iam_role" "read_s3_role" {
+  name = var.role_name
+  permissions_boundary = "arn:aws:iam::113304117666:policy/DefaultBoundaryPolicy"
+  assume_role_policy = jsonencode({
+  
+    "Version": "2012-10-17",
+    
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
     ]
 
   })
 }
 
+resource "aws_iam_role_policy_attachment" "attach" {
+  role = aws_iam_role.read_s3_role.name
+  policy_arn = aws_iam_policy.read_s3_policy.arn
+}
+
 resource "aws_iam_instance_profile" "test_profile" {
   name = "test_profile"
-  role = "${aws_iam_role.read-s3-role.name}"
+  role = "${aws_iam_role.read_s3_role.name}"
 }
